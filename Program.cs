cc85e3397ebe54ed356d49f62a0a38b409af7307
jatoran <adamtbandel@gmail.com>
2020-09-07 21:57:02 -0500

    added c# file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace JimmyShaker {
+    class OperatingSystem {
+        public bool running = true;
+
+        public int cursorRow;
+        public int cursorCol;
+
+        public void getInput() {
+            ConsoleKeyInfo idiot = Console.ReadKey(true);
+            Console.WriteLine(idiot.GetHashCode());
+        }
+    };
+
+    class Program {
+        static void Main(string[] args) { 
+            OperatingSystem OS = new OperatingSystem();
+
+            while (OS.running) {
+                OS.getInput();
+            }
+        }
+    }
+}
