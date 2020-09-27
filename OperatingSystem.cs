09ff6b113514881f2a1e46c31cc68e378009aa93
jatora <adamtbandel@gmail.com>
2020-09-27 03:53:37 -0500

    file upkeep

---
+++ OperatingSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class OperatingSystem {
+    public bool running = true;
+    
+    //initialize in constructor
+    public int cursorRow;
+    public int cursorCol;
+    int displayHeight;
+    int displayWidth;
+    public char[,] screen = new char[10, 10];
+
+    public void getInput() {
+        ConsoleKeyInfo idiot = Console.ReadKey(true);
+        Console.WriteLine(idiot.GetHashCode());
+    }
+
+    public void printScreen() {
+        for (int i = 0; i < displayHeight; i++) {
+            for (int j = 0; j < displayWidth; j++) {
+                Console.Write(screen[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public OperatingSystem(int displayHeight, int displayWidth, int cursorRow, int cursorCol) {
+        this.displayHeight = displayHeight;
+        this.displayWidth = displayWidth;
+        this.cursorRow = cursorRow;
+        this.cursorCol = cursorCol;
+
+        for (int i = 0; i < displayHeight; i++) {
+            for (int j = 0; j < displayWidth; j++) {
+                screen[i, j] = 'd';
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,47 +1,7 @@
 ﻿using System;
 using System.Collections.Specialized;
 
-//get it so we can actually type things that change the state of the screen
-
-namespace commandLineOS {
-    public class OperatingSystem {
-        public bool running = true;
-
-        //initialize in constructor
-        public int cursorRow;
-        public int cursorCol;
-        int displayHeight;
-        int displayWidth;
-        public char[,] screen = new char[10, 10];
-
-        public void getInput() {
-            ConsoleKeyInfo idiot = Console.ReadKey(true);
-            Console.WriteLine(idiot.GetHashCode());
-        }
-
-        public void printScreen() {
-            for (int i = 0; i < displayHeight; i++) {
-                for (int j = 0; j < displayWidth; j++) {
-                    Console.Write(screen[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        public OperatingSystem(int displayHeight, int displayWidth, int cursorRow, int cursorCol) {
-            this.displayHeight = displayHeight;
-            this.displayWidth = displayWidth;
-            this.cursorRow = cursorRow;
-            this.cursorCol = cursorCol;
-
-            for (int i = 0; i < displayHeight; i++) {
-                for (int j = 0; j < displayWidth; j++) {
-                    screen[i, j] = 'd';
-                }
-            }
-        }
-    }
-
+//namespace commandLineOS { //do we need this? program works without it
     class Program {
         static void Main(string[] args) {
             OperatingSystem OS = new OperatingSystem(10,10,0,0);
@@ -52,4 +12,4 @@ namespace commandLineOS {
             }
         }
     }
-}
+//}
