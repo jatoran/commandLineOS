c0a830c32dbc06c66705cc98a79a810626592a86
jatora <adamtbandel@gmail.com>
2020-10-02 02:00:42 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,14 +2,13 @@
 using System.Collections.Specialized;
 
 //namespace commandLineOS { //do we need this? program works without it
-    class Program {
-        static void Main(string[] args) {
-            OperatingSystem OS = new OperatingSystem(10,10,0,0);
+class Program {
+    static void Main(string[] args) {
+        OperatingSystem OS = new OperatingSystem(10,10,0,0);
 
-            while (OS.running) {
-                OS.printScreen();
-                OS.getInput();
-            }
+        while (OS.running) {
+            OS.printScreen();
+            OS.getInput();
         }
     }
-//}
+}
